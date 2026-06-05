e7e369f8acd7fa17d1c5dfda6972fc52e9cd0f05
linpan777 <linpan115300@gmail.com>
2026-06-05 23:30:54 +0800

    update

--- server/app.py
+++ server/app.py
@@ -87,4 +87,4 @@ def get_wiki_content(title):
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, threaded=True)
+    app.run(host="127.0.0.1", port=5002, threaded=True, debug=True)
